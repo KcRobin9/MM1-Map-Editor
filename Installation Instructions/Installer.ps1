76de17202ba905f5d4787c9353ec5e0b4b69be07
KcRobin9 <99972691+KcRobin9@users.noreply.github.com>
2023-10-27 11:00:34 +0200

    Added requirements.txt

--- Installation Instructions/Installer.ps1
+++ Installation Instructions/Installer.ps1
@@ -3,31 +3,26 @@ Start-Sleep -Seconds 2
 python ./Test_Python.py
 Start-Sleep -Seconds 2
 
-Write-Host ""
-Write-Host "Installing required Python Packages..."
-Write-Host ""
-Write-Host "Installing matplotlib (please wait 30 seconds)"
-Write-Host ""
-Start-Sleep -Seconds 2
-Write-Host ""
-pip install matplotlib
-Start-Sleep -Seconds 30
-Write-Host ""
-Write-Host ""
-Write-Host ""
-Write-Host "Installing fake-blender (please wait 30 seconds)"
-Write-Host ""
-pip install fake-bpy-module-latest
-Write-Host ""
-Start-Sleep -Seconds 30 
+# Move up to the root directory (one level up from the current location)
+cd ..
 
-Write-Host "==================================================================="
-Write-Host "==================================================================="
+Write-Host "Installing required Python Packages from requirements.txt..."
+Start-Sleep -Seconds 2
+pip install -r requirements.txt
+
+
+Write-Host ""
+Write-Host "==================================================="
 Write-Host ""
 Write-Host "Installation Complete!"
-Write-Host "***Please press Enter to open the Map Editor and exit this Window***"
 Write-Host ""
-for($i = 30; $i -gt 0; $i--)
+Write-Host "==================================================="
+Write-Host ""
+Write-Host "*** Please press Enter to open the Map Editor and exit this Window ***"
+Write-Host ""
+
+# Wait for the Enter key to be pressed
+for ($i = 20; $i -gt 0; $i--)
 {
     if ($Host.UI.RawUI.KeyAvailable -and ($Host.UI.RawUI.ReadKey("IncludeKeyUp,NoEcho").VirtualKeyCode -eq 13))
     {
@@ -36,9 +31,6 @@ for($i = 30; $i -gt 0; $i--)
     Write-Host "$i..."
     Start-Sleep -Seconds 1
 }
-
-# Move up to the main folder
-cd ..
 
 # Check if 'code' is available in the PATH
 if (Get-Command 'code' -ErrorAction SilentlyContinue) {
