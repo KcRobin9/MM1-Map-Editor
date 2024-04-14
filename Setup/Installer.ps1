211e5e886828d2b5fcd94fb9b77b1a72898442c7
Robin Kisman <99972691+KcRobin9@users.noreply.github.com>
2024-04-15 00:37:42 +0200

    Improved Setup - Automate Installing VS Code Extensions

--- Setup/Installer.ps1
+++ Setup/Installer.ps1
@@ -3,10 +3,26 @@ Start-Sleep -Seconds 2
 python ./Test_Python.py
 Start-Sleep -Seconds 2
 
+
 Write-Host "Running Keybinding_Config.py..."
 Start-Sleep -Seconds 2
 python ./Keybinding_Config.py
 Start-Sleep -Seconds 2
+
+
+# Install VS Code extensions if VS Code CLI is available
+if (Get-Command 'code' -ErrorAction SilentlyContinue) {
+    Write-Host "Installing Visual Studio Code Extensions..."
+	Write-Host ""
+    Start-Sleep -Seconds 2
+    & code --install-extension ms-python.vscode-pylance
+    & code --install-extension jacqueslucke.blender-development
+    & code --install-extension aaron-bond.better-comments
+} else {
+    Write-Host "Visual Studio Code is not detected in PATH. Please ensure it is installed and added to PATH."
+}
+Start-Sleep -Seconds 2
+
 
 # Move up to the root directory (one level up from the current location)
 cd ..
