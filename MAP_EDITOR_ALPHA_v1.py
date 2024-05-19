f073965967b223e8191100980005d399b173dcfe
Robin Kisman <99972691+KcRobin9@users.noreply.github.com>
2024-05-19 15:58:06 +0200

    Refactored Blender Polygon Export Functions

--- MAP_EDITOR_ALPHA_v1.py
+++ MAP_EDITOR_ALPHA_v1.py
@@ -6214,7 +6214,7 @@ def validate_and_extract_bound_number(name: str) -> int:
     elif name.startswith("Shape_"):
         return int(name.split("_")[1])
     else:
-        raise ValueError(f"Unrecognized object name format: {name}")
+        raise ValueError(f"Unrecognized Polygon Name Format: {name}")
     
     
 def extract_polygon_data(obj: bpy.types.Object) -> Dict[str, Union[int, str, bool, list]]:
@@ -6228,7 +6228,8 @@ def extract_polygon_data(obj: bpy.types.Object) -> Dict[str, Union[int, str, boo
         "sort_vertices": obj["sort_vertices"],
         "vertex_coordinates": obj.data.vertices,
         "hud_color": obj["hud_color"],
-        "rotate": obj["rotate"]}
+        "rotate": obj["rotate"]
+        }
     
     return extracted_polygon_data
 
@@ -6240,64 +6241,76 @@ def extract_polygon_texture(obj) -> str:
         if mat and mat.use_nodes:
             for node in mat.node_tree.nodes:
                 if isinstance(node, bpy.types.ShaderNodeTexImage):
-                    return os.path.splitext(node.image.name)[0].replace('.DDS', '').replace('.dds', '')
+                    return os.path.splitext(node.image.name)[0].replace(".DDS", "").replace(".dds", "")
                 
     return Texture.CHECKPOINT  # Default value
 
 
-def export_formatted_polygons(obj: bpy.types.Object) -> str:
-    poly_data = extract_polygon_data(obj)
-    texture_name = extract_polygon_texture(obj).upper() 
-    
+def format_texture_name(texture_name: str) -> str:
     texture_constant = TEXTURE_EXPORT.get(texture_name, texture_name)
-    formatted_texture = next((f'Texture.{name}' for name, value in vars(Texture).items() if value == texture_constant), f'"{texture_name}"')
+    return next((f'Texture.{name}' for name, value in vars(Texture).items() if value == texture_constant), f'"{texture_name}"')
 
-    formatted_vertices = [] 
+
+def format_vertices(vertices: List[bpy.types.MeshVertex]) -> str:
+    formatted_vertices = []
     
-    for vertex in poly_data['vertex_coordinates']:
+    for vertex in vertices:
         transformed_vertex = transform_coordinate_system(vertex.co, blender_to_game = True)
         formatted_vertex = f"({', '.join(format_decimal(comp) for comp in transformed_vertex)})"
         formatted_vertices.append(formatted_vertex)
         
-    vertex_export = ',\n\t\t'.join(formatted_vertices)
+    return ",\n\t\t".join(formatted_vertices)
 
-    optional_variables = []
+
+def gather_optional_variables(poly_data: Dict[str, Union[int, str, bool, list]], obj: bpy.types.Object) -> str:
+    optional_vars = []
     
-    cell_type = CELL_EXPORT.get(str(poly_data['cell_type']), None)
-    if cell_type:
-        optional_variables.append(f"cell_type = {cell_type}")
-    
-    material_index = MATERIAL_EXPORT.get(str(poly_data['material_index']), None)
-    if material_index:
-        optional_variables.append(f"material_index = {material_index}")
-
-    hud_color = HUD_EXPORT.get(next((HUD_IMPORT[i][0] for i, checked in enumerate(obj.hud_colors) if checked), None), None)
-    if hud_color:
-        optional_variables.append(f"hud_color = {hud_color}")
-
-    if poly_data['sort_vertices']:
-        optional_variables.append("sort_vertices = True")
-    if not poly_data['always_visible']:
-        optional_variables.append("always_visible = False")
+    cell_type = CELL_EXPORT.get(str(poly_data["cell_type"]), None)
+    material_index = MATERIAL_EXPORT.get(str(poly_data["material_index"]), None)
+    hud_color = HUD_EXPORT.get(next((HUD_IMPORT[index][0] for index, checked in enumerate(obj.hud_colors) if checked), None), None)
         
-    # Combining optional variables
-    optional_variables_str = ",\n\t".join(optional_variables)
-    if optional_variables_str:
-        optional_variables_str = "\n\t" + optional_variables_str + ","
+    if cell_type:
+        optional_vars.append(f"cell_type = {cell_type}")
+    
+    if material_index:
+        optional_vars.append(f"material_index = {material_index}")
+    
+    if hud_color:
+        optional_vars.append(f"hud_color = {hud_color}")
+
+    if poly_data["sort_vertices"]:
+        optional_vars.append("sort_vertices = True")
+        
+    if not poly_data["always_visible"]:
+        optional_vars.append("always_visible = False")
+        
+    return ",\n\t".join(optional_vars) + ("," if optional_vars else "")
+
+
+def export_formatted_polygons(obj: bpy.types.Object) -> str:
+    poly_data = extract_polygon_data(obj)
+    texture_name = extract_polygon_texture(obj).upper()
+    formatted_texture = format_texture_name(texture_name)
+    formatted_vertices = format_vertices(poly_data["vertex_coordinates"])
+    optional_variables_str = gather_optional_variables(poly_data, obj)
 
     tile_x = obj.get("tile_x", 1)
     tile_y = obj.get("tile_y", 1)
-    rotation = poly_data.get('rotate', 999.0)  
-
+    rotation = poly_data.get("rotate", 999.0)
+    
+    if optional_variables_str:
+        optional_variables_str = f"\n\t{optional_variables_str}"
+    
     template = f"""
 create_polygon(
     bound_number = {poly_data['bound_number']},{optional_variables_str}
     vertex_coordinates = [
-        {vertex_export}])
+        {formatted_vertices}])
 
 save_mesh(
     texture_name = [{formatted_texture}],
-    tex_coords = compute_uv(bound_number = {poly_data['bound_number']}, tile_x = {tile_x:.2f}, tile_y = {tile_y:.2f}, angle_degrees = {rotation:.2f}))"""
+    tex_coords = compute_uv(bound_number = {poly_data['bound_number']}, tile_x = {tile_x:.2f}, tile_y = {tile_y:.2f}, angle_degrees = {rotation:.2f}))
+"""
 
     return template
 
