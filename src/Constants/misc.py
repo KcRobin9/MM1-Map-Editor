f868ab461a115bad0b41f4d9524bee3814205548
Robin <99972691+KcRobin9@users.noreply.github.com>
2025-08-11 15:09:28 +0200

    Expanded Colors Class

--- src/Constants/misc.py
+++ src/Constants/misc.py
@@ -77,27 +77,43 @@ class Threshold:
 
     MESH_VERTEX_COUNT = 16
     VERTEX_INDEX_COUNT = 0x8000  # 32768
- 
-    
+
+
 class Color:
-    RED = (1, 0, 0, 1)
-    GREEN = (0, 1, 0, 1)
-    BLUE = (0, 0, 1, 1)
-    PURPLE = (0.5, 0, 0.5, 1)
-    YELLOW = (1, 1, 0, 1)
-    GOLD = (1, 0.843, 0, 1)
-    WHITE = (1, 1, 1, 1)
+    RED = "#ff0000"
+    RED_LIGHT = "#ff8080"
+    RED_DARK = "#800000"
     
+    GREEN = "#00ff00"
+    GREEN_LIGHT = "#80ff80"
+    GREEN_DARK = "#008000"
+    
+    BLUE = "#0000ff"
+    BLUE_LIGHT = "#8080ff"
+    BLUE_DARK = "#000080"  # Dading: #070644
+    
+    PURPLE = "#800080"
+    PURPLE_LIGHT = "#c080c0"
+    PURPLE_DARK = "#400040"
+    
+    YELLOW = "#ffff00"
+    YELLOW_LIGHT = "#ffff80"
+    YELLOW_DARK = "#808000"
+    
+    GOLD = "#ffd700"
+    GOLD_LIGHT = "#ffeb80"
+    GOLD_DARK = "#806b00"
+    
+    WHITE = "#ffffff"
+    WHITE_DARK = "#cccccc"  # Light gray
+
+    ORANGE = "#ffa500"
+
     WOOD = "#7b5931"
     SNOW = "#cdcecd"
     WATER = "#5d8096"
     ROAD = "#414441"
     GRASS = "#396d18"
-
-    ORANGE = "#ffa500"
-    RED_DARK = "#af0000"
-    RED_LIGHT = "#ff7f7f"
-    YELLOW_LIGHT = "#ffffe0"
 
     IND_WALL = "#7b816a"
     BRICKS_MALL = "#e6cab4"
